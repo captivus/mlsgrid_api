a0d262340574f1ded7cd3dac86c3b15b16103ccf
captivus <366332+captivus@users.noreply.github.com>
2022-09-14 14:33:26 -0500

    Initial and ongoing replication confirmed. OpenHouse resource temporarily not supported.

--- mlsgrid.py
+++ mlsgrid.py
@@ -1,9 +1,40 @@
 from mlsgrid_api import MLSGridAPI
 
+# Testing in DEBUG mode
 mred = MLSGridAPI(debug=True)
-#mred._DEBUG_file_storage_cleanup()
-#mred.replicate_property(initial=True)
+mred._DEBUG_file_storage_cleanup()
+
+# Test replication of Property resource
+mred.replicate_property(initial=True)
 mred.replicate_property()
 
-#mred.replicate_property(resource_name='Property', initial=True)
-#print(mred.get_latest_timestamp())
+# Test replication of Member resource
+mred.replicate_member(initial=True)
+mred.replicate_member()
+
+# Test repliication of Office resource
+mred.replicate_office(initial=True)
+mred.replicate_office()
+
+
+# # Testing outside of DEBUG mode
+# mred = MLSGridAPI()
+# mred._DEBUG_file_storage_cleanup()
+
+# # Test replication of Property resource
+# mred.replicate_property(initial=True)
+# mred.replicate_property()
+
+# # Test replication of Member resource
+# mred.replicate_member(initial=True)
+# mred.replicate_member()
+
+# # Test repliication of Office resource
+# mred.replicate_office(initial=True)
+# mred.replicate_office()
+
+
+# Waiting for full implementation ...
+# Test replication of OpenHouse resource
+# mred.replicate_openhouse(initial=True)
+# mred.replicate_openhouse()
--- mlsgrid_api.py
+++ mlsgrid_api.py
@@ -24,6 +24,7 @@ class MLSGridAPI():
         self.PROPERTY_URL = self.MLSGRID_API_URL + 'Property/'
         self.MEMBER_URL = self.MLSGRID_API_URL + 'Member/'
         self.OFFICE_URL = self.MLSGRID_API_URL + 'Office/'
+        # TODO: complete support for OpenHouse replication (writing of records doesn't yet work)
         self.OPENHOUSE_URL = self.MLSGRID_API_URL + 'OpenHouse/'
 
         # Expand all resources by default (user can override)
@@ -46,7 +47,7 @@ class MLSGridAPI():
         self.DEBUG_REPLICATE_ITERATIONS = 3
 
         # Output file configuration
-        self.OUTPUT_FILE_SUFFIX = '_output_file.json'
+        self.OUTPUT_FILE_PREFIX = '_output_file_'
 
         # Latest ModificationTimestamp
         # TODO: remove this starter value -- used only in testing prior to DB setup
@@ -63,11 +64,12 @@ class MLSGridAPI():
     def set_modification_timestamp(self, modification_timestamp=None, resource_name=None):
         # MLSGrid API docs say that they sort responses by ModificationTimestamp
         # This means we can safely save the last that we process as the latest
-        timestamp_file = resource_name + '_modification_timestamp.txt'
+        timestamp_file = '_modification_timestamp_' + resource_name + '.txt'
         
         with open(file=timestamp_file, mode='w+') as f:
             f.write(modification_timestamp)            
         self.modification_timestamp[resource_name] = modification_timestamp
+        if self.DEBUG:  print('New ModificationTimestamp = ' + self.modification_timestamp[resource_name])
 
     def create_session(self):
         # Configuration
@@ -110,11 +112,18 @@ class MLSGridAPI():
         #       * Advise of errors (and their contents)
         #   * Add $top / max_records support later ... the API knows how to manage this in the interim
 
+        # if initial == True:
+        #     URL = '{resource_name_url}?$filter=OriginatingSystemName eq {mls_system} and MlgCanView eq true&$expand={expand}'.format(
+        #         resource_name_url=resource_name_url,
+        #         mls_system=self.MLS_SYSTEM,
+        #         expand=self.EXPAND
+        #     )
+        
         if initial == True:
-            URL = '{resource_name_url}?$filter=OriginatingSystemName eq {mls_system} and MlgCanView eq true&$expand={expand}'.format(
+            URL = '{resource_name_url}?$filter=OriginatingSystemName eq {mls_system} and MlgCanView eq true{expand}'.format(
                 resource_name_url=resource_name_url,
                 mls_system=self.MLS_SYSTEM,
-                expand=self.EXPAND
+                expand='' if resource_name == 'OpenHouse' else '&$expand=' + self.EXPAND
             )
         
         else:
@@ -127,34 +136,10 @@ class MLSGridAPI():
 
         # TESTING:  Limit number of records returned
         if self.DEBUG:
-            #URL = URL + '&$top=2'
             URL = URL + '&$top=' + str(self.DEBUG_MAX_RECORDS)
             print('API Request URL = ' + URL)
         
         replication_iterations = 0
-        
-        # Replicate until finished
-        # while URL != 'Finished':
-        #     # Break if in debug mode and we've replicated as many 
-        #     if self.DEBUG and replication_iterations >= self.DEBUG_REPLICATE_ITERATIONS:
-        #         break
-        #     # Post GET request to API
-        #     response = session.get(url=URL)
-        #     if self.DEBUG:  print(response)
-        #     # Write records to database
-        #     self.write_records(records=response.json()['value'], resource_name=resource_name, output_to='file')
-
-        #     # set next_link to enable looping
-        #     try:
-        #         next_link = response.json()['@odata.nextLink']
-            
-        #     except KeyError:
-        #         next_link = 'Finished'
-
-        #     if self.DEBUG:  print('next_link = ' + next_link)
-            
-        #     replication_iterations = replication_iterations +1
-        #     URL = next_link
 
         while URL != 'Finished':
             # Break if in debug mode and we've replicated as many 
@@ -173,6 +158,7 @@ class MLSGridAPI():
         if self.DEBUG:  print(response)
 
         # Write records to database
+        # FIXME:  for OpenHouse records, as this JSON schema isn't returned
         self.write_records(records=response.json()['value'], resource_name=resource_name, output_to='file')
 
         # set next_link to enable looping
@@ -200,9 +186,10 @@ class MLSGridAPI():
         '''Replicates the Office resource of the MLSGrid API'''
         self._replicate(resource_name='Office', initial=initial, session=session, next_link=next_link)
 
-    def replicate_openhouse(self, initial=False, session=None, next_link=None):
-        '''Replicates the OpenHouse resource of the MLSGrid API'''
-        self._replicate(resource_name='OpenHouse', initial=initial, session=session, next_link=next_link)
+    # TODO: complete support for OpenHouse replication
+    # def replicate_openhouse(self, initial=False, session=None, next_link=None):
+    #     '''Replicates the OpenHouse resource of the MLSGrid API'''
+    #     self._replicate(resource_name='OpenHouse', initial=initial, session=session, next_link=next_link)
 
     # TODO: tailor output files for each resource
     # TODO: overwrite output file on initial replication, otherwise expect output file to exist
@@ -210,7 +197,7 @@ class MLSGridAPI():
         if output_to == 'file':
             # We're writing to a file that already exists
 
-            output_file = resource_name + self.OUTPUT_FILE_SUFFIX
+            output_file = self.OUTPUT_FILE_PREFIX + resource_name + '.json'
 
             if os.path.exists(output_file):
                 with open(file=output_file, mode='r') as json_infile:
@@ -223,7 +210,6 @@ class MLSGridAPI():
                     json.dump(obj=json_db, fp=json_outfile, indent=4)
                 print('Wrote ' + str(len(records)) + ' records!')
                 self.set_modification_timestamp(records[len(records)-1]['ModificationTimestamp'], resource_name=resource_name)
-                if self.DEBUG:  print('New ModificationTimestamp = ' + self.modification_timestamp[resource_name])
 
             # This is the first time we're writing to the file
             if not os.path.exists(output_file):
@@ -231,7 +217,6 @@ class MLSGridAPI():
                     json.dump(obj=records, fp=json_outfile, indent=4)
                 print('Wrote ' + str(len(records)) + ' records!')
                 self.set_modification_timestamp(records[len(records)-1]['ModificationTimestamp'], resource_name=resource_name)
-                if self.DEBUG:  print('New ModificationTimestamp = ' + self.modification_timestamp[resource_name])
         
         elif output_to == 'database':
             pass
@@ -244,12 +229,12 @@ class MLSGridAPI():
         # Next iteration returning the internal property set when saving records
         #return self.modification_timestamp
 
-        output_file = resource_name + self.OUTPUT_FILE_SUFFIX
+        output_file = self.OUTPUT_FILE_PREFIX + resource_name + '.json'
 
         # Now Open JSON output file and pull latest ModificationTimestamp
         with open(file=output_file, mode='r') as json_infile:
             json_db = json.load(fp=json_infile)
-        print(json_db[len(json_db)-1])
+        #print(json_db[len(json_db)-1])
 
         return json_db[len(json_db) - 1]['ModificationTimestamp']
 
@@ -257,7 +242,7 @@ class MLSGridAPI():
     def _DEBUG_file_storage_cleanup(self):
         # Delete output files & modification timestamp files
         for key in self.modification_timestamp.keys():
-            f = key + self.OUTPUT_FILE_SUFFIX
+            f = self.OUTPUT_FILE_PREFIX + key + '.json'
             if os.path.exists(f):   os.remove(f)
-            f = key + '_modification_timestamp.txt'
+            f = '_modification_timestamp_' + key + '.txt'
             if os.path.exists(f):   os.remove(f)
